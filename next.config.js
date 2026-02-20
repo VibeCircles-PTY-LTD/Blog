5b2b7eff917f1170dc3d593e2199e269fede6d00
VibesCircles <vibecircless@gmail.com>
2026-02-20 22:06:04 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {
+    // Ensure Turbopack uses this project root (avoid parent lockfile confusion).
+    root: __dirname,
+  },
 };
 
 module.exports = nextConfig;
